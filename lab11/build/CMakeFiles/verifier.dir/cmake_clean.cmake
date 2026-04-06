2b2a61462709c37b206a5ade89520e26e58d4974
vaa90 <vaa9@sfu.ca>
2026-04-05 18:43:00 -0700

    lab 11

---
+++ lab11/build/CMakeFiles/verifier.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/verifier.dir/lab11.c.o"
+  "CMakeFiles/verifier.dir/lab11.c.o.d"
+  "verifier"
+  "verifier.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/verifier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
